65d3e16605f5a2a450c2bd840dced84ce3e93191
Vitaly Kotov <catvitalio@gmail.com>
2024-10-27 21:49:17 +0700

    improve performance for SendMailService

--- mails/services/send_mail.py
+++ mails/services/send_mail.py
@@ -1,3 +1,4 @@
+import asyncio
 from email.message import EmailMessage
 
 from config.jinja import jinja_env
@@ -8,23 +9,10 @@ from dtos import SendMailDTO
 
 class SendMailService:
     async def send(self, dto: SendMailDTO) -> None:
-        """
-        Send mails to recipients
+        messages = self._get_messages(dto)
+        asyncio.gather(*[self._handle_message(message) for message in messages])
 
-        Additionally, the SMTP protocol is not designed to be used in parallel. Multiple commands
-        must be sent to send an email, and they must be sent in the correct sequence. As a
-        consequence of this, executing multiple SMTP.send_message() tasks in parallel
-        (i.e. with asyncio.gather()) is not any more efficient than executing in sequence, as the
-        client must wait until one mail is sent before beginning the next.
-
-        https://aiosmtplib.readthedocs.io/en/latest/client.html
-        """
-        async with smtp_client:
-            messages = await self._get_messages(dto)
-            for message in messages:
-                await smtp_client.send_message(message)
-
-    async def _get_messages(self, dto: SendMailDTO) -> list[EmailMessage]:
+    def _get_messages(self, dto: SendMailDTO) -> list[EmailMessage]:
         messages = []
         content = jinja_env.get_template(dto.type.template).render(**dto.context)
 
@@ -38,3 +26,7 @@ class SendMailService:
             messages.append(message)
 
         return messages
+
+    async def _handle_message(self, message: EmailMessage) -> None:
+        async with smtp_client:
+            await smtp_client.send_message(message)
